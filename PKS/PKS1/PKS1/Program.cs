11f466f1ec69db9c73638146a47dd15327fe4f5f
nilixin <nilixen@yandex.ru>
2022-02-17 10:42:54 +0300

    Some experiments

--- PKS/PKS1/PKS1/Program.cs
+++ PKS/PKS1/PKS1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using Tftp.Net;
 
 namespace v1
 {
@@ -8,8 +10,35 @@ namespace v1
     {
         static async Task Main(string[] args)
         {
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://127.0.0.1/file.txt");
-            request.Credentials = new NetworkCredential("User2", "1231");
+            //// connect
+            //FtpWebRequest connectRequest = (FtpWebRequest)WebRequest.Create("ftp://localhost");
+            //connectRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+            //connectRequest.Credentials = new NetworkCredential("User", "");
+
+            //FtpWebResponse connectResponse = (FtpWebResponse)connectRequest.GetResponse();
+            //Console.WriteLine("code " + connectResponse.StatusCode.ToString());
+            //connectResponse.Close();
+
+
+            //// tryout
+            //FtpWebRequest commandRequest = (FtpWebRequest)WebRequest.Create("ftp://localhost/folder");
+            ////commandRequest.Method = "APPE filez.txt";
+            //commandRequest.Method = "LIST";
+            //commandRequest.Credentials = new NetworkCredential("User", "");
+
+
+
+            //FtpWebResponse commandResponse = (FtpWebResponse)commandRequest.GetResponse();
+            //Console.WriteLine("code " + commandResponse.StatusCode.ToString());
+            //commandResponse.Close();
+
+
+
+            IPAddress ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            TftpClient client = new TftpClient(ip);
+
+            //Console.WriteLine(client.ToString());
+
 
             Console.ReadKey();
         }
