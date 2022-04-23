cc12f6e3c8170207421850c1b5de00f952f3562f
nilixin <nilixen@yandex.ru>
2022-04-24 02:21:19 +0300

    UDP sockets program started

---
+++ PKS/PKS3/PKS3/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace PKS3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.bSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLocalPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbRemoteAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbRemotePort = new System.Windows.Forms.TextBox();
+            this.bCheck = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(12, 259);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbMessage.Size = new System.Drawing.Size(281, 144);
+            this.tbMessage.TabIndex = 0;
+            // 
+            // bSend
+            // 
+            this.bSend.Location = new System.Drawing.Point(12, 409);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(281, 29);
+            this.bSend.TabIndex = 1;
+            this.bSend.Text = "Отправить";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.bSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сообщение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Локальный порт";
+            // 
+            // tbLocalPort
+            // 
+            this.tbLocalPort.Location = new System.Drawing.Point(12, 32);
+            this.tbLocalPort.Name = "tbLocalPort";
+            this.tbLocalPort.Size = new System.Drawing.Size(281, 27);
+            this.tbLocalPort.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Удалённый адрес";
+            // 
+            // tbRemoteAddress
+            // 
+            this.tbRemoteAddress.Location = new System.Drawing.Point(12, 85);
+            this.tbRemoteAddress.Name = "tbRemoteAddress";
+            this.tbRemoteAddress.Size = new System.Drawing.Size(281, 27);
+            this.tbRemoteAddress.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Удалённый порт";
+            // 
+            // tbRemotePort
+            // 
+            this.tbRemotePort.Location = new System.Drawing.Point(12, 138);
+            this.tbRemotePort.Name = "tbRemotePort";
+            this.tbRemotePort.Size = new System.Drawing.Size(281, 27);
+            this.tbRemotePort.TabIndex = 7;
+            // 
+            // bCheck
+            // 
+            this.bCheck.Location = new System.Drawing.Point(12, 171);
+            this.bCheck.Name = "bCheck";
+            this.bCheck.Size = new System.Drawing.Size(281, 29);
+            this.bCheck.TabIndex = 9;
+            this.bCheck.Text = "Проверить";
+            this.bCheck.UseVisualStyleBackColor = true;
+            this.bCheck.Click += new System.EventHandler(this.bCheckConnection_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(321, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Чят";
+            // 
+            // tbChat
+            // 
+            this.tbChat.Location = new System.Drawing.Point(321, 32);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbChat.Size = new System.Drawing.Size(467, 406);
+            this.tbChat.TabIndex = 10;
+            this.tbChat.Text = "blank";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbChat);
+            this.Controls.Add(this.bCheck);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbRemotePort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbRemoteAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbLocalPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bSend);
+            this.Controls.Add(this.tbMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tbMessage;
+        private Button bSend;
+        private Label label1;
+        private Label label2;
+        private TextBox tbLocalPort;
+        private Label label3;
+        private TextBox tbRemoteAddress;
+        private Label label4;
+        private TextBox tbRemotePort;
+        private Button bCheck;
+        private Label label5;
+        private TextBox tbChat;
+    }
+}
---
+++ PKS/PKS3/PKS3/UdpClientLogic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKS3
+{
+    public class UdpClientLogic
+    {
+        int LocalPort;
+        IPAddress RemoteIp;
+        int RemotePort;
+
+        public UdpClientLogic(int localPort, IPAddress remoteIp, int remotePort)
+        {
+            LocalPort = localPort;
+            RemoteIp = remoteIp;
+            RemotePort = remotePort;
+        }
+
+        public string? Connect()
+        {
+            UdpClient udpClient = new UdpClient();
+            try
+            {
+                udpClient.Connect(RemoteIp, RemotePort);
+                return udpClient.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+        }
+
+        public string Receive()
+        {
+            UdpClient receiver = new(LocalPort);
+            IPEndPoint remoteAddress = null;
+
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteAddress);
+                    string message = Encoding.UTF8.GetString(data);
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
