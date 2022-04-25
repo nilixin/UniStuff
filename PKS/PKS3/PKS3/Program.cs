4e998578287a9839af4f8ec3f1374d737bcbfa24
nilixin <nilixen@yandex.ru>
2022-04-25 23:24:10 +0300

    UDP client done

--- PKS/PKS3/PKS3/Program.cs
+++ PKS/PKS3/PKS3/Program.cs
@@ -1,17 +1,122 @@
-namespace PKS3
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UdpClientApp
 {
-    internal static class Program
+    class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static string RemoteAddress; // хост для отправки данных
+        static int RemotePort; // порт для отправки данных
+        static int LocalPort; // локальный порт для прослушивания входящих подключений
+
+        static void Main(string[] args)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            try
+            {
+                // Локальный порт
+                bool localPortCorrect = false;
+                while (!localPortCorrect)
+                {
+                    Console.Write("Введите порт для прослушивания: ");
+                    try
+                    {
+                        LocalPort = int.Parse(Console.ReadLine());
+                        localPortCorrect = true;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Ошибка. Нечисловое значение.");
+                    }
+                }
+
+                // Удалённый адрес
+                bool remoteAddressCorrect = false;
+                while (!remoteAddressCorrect)
+                {
+                    Console.Write("Введите удаленный адрес для подключения: ");
+                    try
+                    {
+                        RemoteAddress = Console.ReadLine();
+                        remoteAddressCorrect = true;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Ошибка. Проверьте правильность ввода адреса.");
+                    }
+                }
+
+                // Удалённый порт
+                bool remotePortCorrect = false;
+                while (!remotePortCorrect)
+                {
+                    Console.Write("Введите удаленный порт для подключения: ");
+                    try
+                    {
+                        RemotePort = int.Parse(Console.ReadLine());
+                        remotePortCorrect = true;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Ошибка. Нечисловое значение.");
+                    }
+                }
+
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+                Console.WriteLine("Данные приняты.");
+                SendMessage(); // отправляем сообщение
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void SendMessage()
+        {
+            UdpClient sender = new UdpClient(); // создаем UdpClient для отправки сообщений
+            try
+            {
+                while (true)
+                {
+                    string message = Console.ReadLine(); // сообщение для отправки
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    sender.Send(data, data.Length, RemoteAddress, RemotePort); // отправка
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+
+        private static void ReceiveMessage()
+        {
+            UdpClient receiver = new UdpClient(LocalPort); // UdpClient для получения данных
+            IPEndPoint remoteIp = null; // адрес входящего подключения
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp); // получаем данные
+                    string message = Encoding.Unicode.GetString(data);
+                    Console.WriteLine("Собеседник: {0}", message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
         }
     }
 }
