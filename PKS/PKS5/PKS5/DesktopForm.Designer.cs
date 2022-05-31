95c11cc33ff30738d9736cf1415c7ee1b880b148
nilixin <nilixen@yandex.ru>
2022-05-31 03:24:25 +0300

    Primal VNC app

---
+++ PKS/PKS5/PKS5/DesktopForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace PKS5
+{
+    partial class DesktopForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdDesktop = new VncSharp.RemoteDesktop();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bConnect = new System.Windows.Forms.Button();
+            this.bDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rdDesktop
+            // 
+            this.rdDesktop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rdDesktop.AutoScroll = true;
+            this.rdDesktop.AutoScrollMinSize = new System.Drawing.Size(608, 427);
+            this.rdDesktop.Location = new System.Drawing.Point(12, 68);
+            this.rdDesktop.Name = "rdDesktop";
+            this.rdDesktop.Size = new System.Drawing.Size(868, 443);
+            this.rdDesktop.TabIndex = 0;
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(12, 28);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(190, 22);
+            this.tbHost.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Хост (localhost)";
+            // 
+            // bConnect
+            // 
+            this.bConnect.Location = new System.Drawing.Point(208, 28);
+            this.bConnect.Name = "bConnect";
+            this.bConnect.Size = new System.Drawing.Size(148, 23);
+            this.bConnect.TabIndex = 5;
+            this.bConnect.Text = "Подключиться";
+            this.bConnect.UseVisualStyleBackColor = true;
+            this.bConnect.Click += new System.EventHandler(this.bConnect_Click);
+            // 
+            // bDisconnect
+            // 
+            this.bDisconnect.Location = new System.Drawing.Point(362, 29);
+            this.bDisconnect.Name = "bDisconnect";
+            this.bDisconnect.Size = new System.Drawing.Size(148, 23);
+            this.bDisconnect.TabIndex = 6;
+            this.bDisconnect.Text = "Отключиться";
+            this.bDisconnect.UseVisualStyleBackColor = true;
+            this.bDisconnect.Click += new System.EventHandler(this.bDisconnect_Click);
+            // 
+            // DesktopForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 523);
+            this.Controls.Add(this.bDisconnect);
+            this.Controls.Add(this.bConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbHost);
+            this.Controls.Add(this.rdDesktop);
+            this.Name = "DesktopForm";
+            this.Text = "DesktopForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private VncSharp.RemoteDesktop rdDesktop;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bConnect;
+        private System.Windows.Forms.Button bDisconnect;
+    }
+}
+
---
+++ PKS/PKS5/PKS5/DesktopForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PKS5
+{
+    public partial class DesktopForm : Form
+    {
+        public DesktopForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            // Ввод данных
+            string host;
+
+            if (string.IsNullOrEmpty(tbHost.Text))
+                host = "localhost";
+            else
+                host = tbHost.Text;
+            
+            // Подключение
+            try
+            {
+                rdDesktop.Connect(host);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rdDesktop.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
